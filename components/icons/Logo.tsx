b5549328106fce9705ae8bd160c52b174d0d32eb
Yo i'm Alfred <81571639+AlfredWilliams@users.noreply.github.com>
2025-08-22 11:51:55 +0000

    Update Logo.tsx

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../assets/atlogo.png';
 const Logo: React.FC<{ className?: string }> = ({ className }) => (
   <img
-    src="../../assets/atlogo"
+    src="../../assets/atlogo.png"
     alt="AT Logo"
     className={className}
   />
