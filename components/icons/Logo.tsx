1e4627964940c76a44c5f25288f90c74642b576f
Yo i'm Alfred <81571639+AlfredWilliams@users.noreply.github.com>
2025-08-22 11:49:08 +0000

    at logo retry

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import '../../assets/atlogo.png';
 const Logo: React.FC<{ className?: string }> = ({ className }) => (
   <img
-       src="../../assets/atlogo.png"
+    src="../../assets/atlogo"
     alt="AT Logo"
     className={className}
   />
